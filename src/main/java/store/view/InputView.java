165185c49b0f40b350dacb067da9d9b696b2cfcc
lucku <lsw6731@naver.com>
2024-11-10 21:32:01 +0900

    feat: 프로모션 부족 수량 정가 결제 여부 입력 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,12 +8,15 @@ import store.dto.OrderNotice;
 import store.dto.PurchaseInfo;
 
 import static store.constant.ErrorMessages.INVALID_FORMAT_MESSAGE;
+import static store.constant.ErrorMessages.INVALID_INPUT_MESSAGE;
 
 public class InputView {
     private static final String PURCHASE_PROMPT = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String ADDITIONAL_PURCHASE_PROMPT = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
     private static final String PROMOTION_AVAILABLE_PROMPT =
             "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+    private static final String PROMOTION_STOCK_INSUFFICIENT_PROMPT =
+            "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
     private static final String ORDER_DELIMITER = ",";
     private static final String NAME_QUANTITY_DELIMITER = "-";
     private static final String EMPTY_STRING = "";
@@ -30,6 +33,15 @@ public class InputView {
     public boolean askAddFreeProduct(OrderNotice orderNotice) {
         printPrompt(String.format(
                 PROMOTION_AVAILABLE_PROMPT,
+                orderNotice.productName(),
+                orderNotice.quantity()
+        ));
+        return getUserConfirmation();
+    }
+
+    public boolean askPurchaseWithoutPromotion(OrderNotice orderNotice) {
+        printPrompt(String.format(
+                PROMOTION_STOCK_INSUFFICIENT_PROMPT,
                 orderNotice.productName(),
                 orderNotice.quantity()
         ));
