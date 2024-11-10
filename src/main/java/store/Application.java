41d73d8df18b6431e52217f50d89f8e8eda986da
lucku <lsw6731@naver.com>
2024-11-10 21:33:18 +0900

    feat: Application config 초기설정 클래스로 분리

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,18 @@
 package store;
 
+import java.io.IOException;
+import store.config.ApplicationConfig;
+import store.controller.StoreController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            ApplicationConfig config = new ApplicationConfig();
+            StoreController storeController = config.storeController();
+
+            storeController.run();
+        } catch (IOException e) {
+            System.out.println("resources 파일 오류 발생: " + e.getMessage());
+        }
     }
 }
