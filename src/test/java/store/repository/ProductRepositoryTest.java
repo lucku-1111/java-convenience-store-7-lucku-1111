6a2bf007014848ce3a9eb2680d60015dd546a4a5
lucku <lsw6731@naver.com>
2024-11-11 23:15:24 +0900

    test: 추가된 기능 테스트 코드 추가

--- src/test/java/store/repository/ProductRepositoryTest.java
+++ src/test/java/store/repository/ProductRepositoryTest.java
@@ -9,6 +9,21 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductRepositoryTest {
+
+    @DisplayName("프로모션 제품만 존재할 때 일반 제품이 추가된다.")
+    @Test
+    void 일반_제품_자동_추가_테스트() {
+        Product promotionalProduct = new Product("콜라", 1000, 10, "탄산2+1");
+        ProductRepository repository = new ProductRepository(List.of(promotionalProduct));
+
+        Optional<Product> regularProduct = repository.findRegularProduct("콜라");
+
+        assertThat(regularProduct).isPresent();
+        assertThat(regularProduct.get().getName()).isEqualTo("콜라");
+        assertThat(regularProduct.get().getQuantity()).isEqualTo(0);
+        assertThat(regularProduct.get().getPromotion()).isEmpty();
+    }
+
     @DisplayName("모든 상품을 반환한다.")
     @Test
     void 모든_제품_반환_테스트() {
@@ -17,8 +32,9 @@ class ProductRepositoryTest {
         ProductRepository repository = new ProductRepository(List.of(product1, product2));
 
         List<Product> products = repository.findAll();
+        Product product3 = repository.findRegularProduct("콜라").orElse(null);
 
-        assertThat(products).containsExactlyInAnyOrder(product1, product2);
+        assertThat(products).containsExactlyInAnyOrder(product1, product2, product3);
     }
 
     @DisplayName("프로모션 제품을 이름으로 조회한다.")
